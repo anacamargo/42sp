3a6683cf9246a9edfd9f3397c0ce1cb2b146c0fb
anacamargo <anaclaudia2501@gmail.com>
2021-02-13 20:51:23 +0100

    files

---
+++ libft/ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aclaudia <aclaudia@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 18:05:49 by aclaudia          #+#    #+#             */
+/*   Updated: 2021/02/13 19:33:42 by aclaudia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*str;
+	size_t				i;
+
+	str = s;
+	i = 0;
+
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((char*)s + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aclaudia <aclaudia@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 05:46:39 by aclaudia          #+#    #+#             */
+/*   Updated: 2021/02/13 18:47:45 by aclaudia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	const unsigned char *str1;
+	const unsigned char *str2;
+
+	str1 = s1;
+	str2 = s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return(str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
