bb7bf0011003c9593fd99f7c7f4dee8b86b5ac0c
anacamargo <anaclaudia2501@gmail.com>
2021-02-25 04:08:51 +0100

    fix norma

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,20 +6,19 @@
 /*   By: aclaudia <aclaudia@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 03:44:32 by aclaudia          #+#    #+#             */
-/*   Updated: 2021/02/25 04:06:25 by aclaudia         ###   ########.fr       */
+/*   Updated: 2021/02/25 04:08:09 by aclaudia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*str;
 	int		len;
 	int		i;
 
 	len = strlen(s);
-
 	str = (char*)malloc(len * sizeof(char));
 	i = 0;
 	if (!str)
@@ -29,5 +28,5 @@ char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		str[i] = f(i, s[i]);
 		i++;
 	}
-	return (str);	
+	return (str);
 }
