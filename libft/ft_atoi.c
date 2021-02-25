d3dae11574ad4ccb2d5b696496b13ee1bc7454a6
anacamargo <anaclaudia2501@gmail.com>
2021-02-25 22:11:47 +0100

    add files

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,19 @@
 /*   By: aclaudia <aclaudia@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 19:10:00 by aclaudia          #+#    #+#             */
-/*   Updated: 2021/02/25 03:33:17 by aclaudia         ###   ########.fr       */
+/*   Updated: 2021/02/25 22:03:02 by aclaudia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_ispace(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\v' \
+	|| c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *nptr)
 {
@@ -21,13 +29,15 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	n = 0;
 	sign = 1;
-	if (nptr[i] == '-' || nptr[i] == '+' || nptr[i] == ' ')
+	while (ft_ispace(nptr[i]))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
 			sign = -1;
 		i++;
 	}
-	while (nptr[i] > '0' && nptr[i] < '9')
+	while (ft_isdigit(nptr[i]))
 	{
 		n = n * 10 + nptr[i] - '0';
 		i++;
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,53 +1,39 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   ft_memmove.c                                       :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: aclaudia <aclaudia@student.42sp.org.br>    +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2021/02/13 03:58:57 by aclaudia          #+#    #+#             */
-// /*   Updated: 2021/02/13 18:32:17 by aclaudia         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aclaudia <aclaudia@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 16:03:41 by aclaudia          #+#    #+#             */
+/*   Updated: 2021/02/25 16:14:20 by aclaudia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
-	unsigned char		*dn;
-	const unsigned char	*sn;
-	size_t				j;
-	size_t 				i;
+	size_t				i;
 
 	d = dest;
 	s = src;
-	dn = dest + n;
-	sn = src + n;
-	i = 0;
-	j = 0;
-
+	i = dest < src ? 0 : n - 1;
 	if (!dest && !src)
 		return (NULL);
-
 	if (dest < src)
-	{
 		while (i < n)
 		{
 			d[i] = s[i];
-			i++; 
+			i++;
 		}
-	}
 	else
-	{
-		while (n > 0)
+		while (i != (size_t)-1)
 		{
-			dn[j] = sn[j];
-			j--;
+			d[i] = s[i];
+			i--;
 		}
-	}
 	return (dest);
 }
