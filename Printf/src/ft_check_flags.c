481e459d443eaa22a37a0cf4f1c4d2a420e50ec4
Ana Camargo <anaclaudia2501@gmail.com>
2021-03-27 21:07:05 -0300

    flag

--- Printf/src/ft_check_flags.c
+++ Printf/src/ft_check_flags.c
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 04:31:46 by aclaudia          #+#    #+#             */
-/*   Updated: 2021/03/27 18:52:42 by vcordeir         ###   ########.fr       */
+/*   Updated: 2021/03/27 21:06:08 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,20 @@ static	void	deal_digit(char **s, t_flags **value)
 		if (**s == '0')
 			(*value)->zero = 1;
 		while (ft_isdigit(**s))
+		{
 			(*value)->width = (*value)->width * 10 + \
-				ft_atoi(ft_substr(*s++, 0, 1));
+				ft_atoi(ft_substr(*s, 0, 1));
+			*s += 1;
+		}
 	}
 	else
 	{
 		while (ft_isdigit(**s))
+		{
 			(*value)->precision = (*value)->precision * 10 + \
-				ft_atoi(ft_substr(*s++, 0, 1));
+				ft_atoi(ft_substr(*s, 0, 1));
+			*s += 1;
+		}
 	}
 	*s -= 1;
 }
--- Printf/src/ft_printf.c
+++ Printf/src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 04:31:46 by aclaudia          #+#    #+#             */
-/*   Updated: 2021/03/27 20:47:31 by vcordeir         ###   ########.fr       */
+/*   Updated: 2021/03/27 21:06:30 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,21 @@ int		ft_printf(const char *format, ...)
 				value.count++;
 				value.index++;
 			}
-			// printf("%s\n", ft_substr(format, (value.index - value.count), value.count));
 			if (value.count)
 				ft_check_flags(ft_substr(format, (value.index - value.count), \
-					value.count), &value);
+					value.count + 1), &value);
 			ft_select_conversion(format[value.index], &value, &print);
 
 			/* -------------------------------------------------------------- */
 			/* ------------------------- TEST FLAGS ------------------------- */
 			/* -------------------------------------------------------------- */
-			printf("index: %d\n", value.index);
-			printf("count: %d\n", value.count);
-			printf("dot: %d\n", value.dot);
-			printf("minus: %d\n", value.minus);
-			printf("precision: %d\n", value.precision);
-			printf("width: %d\n", value.width);
-			printf("zero: %d\n", value.zero);
+			// printf("index: %d\n", value.index);
+			// printf("count: %d\n", value.count);
+			// printf("dot: %d\n", value.dot);
+			// printf("minus: %d\n", value.minus);
+			// printf("precision: %d\n", value.precision);
+			// printf("width: %d\n", value.width);
+			// printf("zero: %d\n", value.zero);
 			/* -------------------------------------------------------------- */
 			/* -------------------------------------------------------------- */
 
@@ -74,7 +73,7 @@ int main()
 	// char c = 'a';
 	// ft_printf("TESTE\n%c\n%d\n%s\n%u\n%x\n%X\n0x%p\n", 'a', 1000, "oi", -4, 1034, 25963, &c);
 	// printf("%p\n", &c);
-	ft_printf("%*.4245u\n", -234, 4573);
+	ft_printf("%*.42u\n", -234, 4573);
 	// ft_putnbr_fd(1000, 1);
 	return(0);
 }
