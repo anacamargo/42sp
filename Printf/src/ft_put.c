1588ecdeedbf446bc4d3b030c7b55358ce80e814
Ana Camargo <anaclaudia2501@gmail.com>
2021-03-27 22:47:29 -0300

    v6

--- Printf/src/ft_put.c
+++ Printf/src/ft_put.c
@@ -35,6 +35,32 @@ int		ft_print_str(t_flags *value, t_print *print)
 		print->size)) : ft_max(value->width, print->size));
 }
 
+int		ft_print_uint(t_flags *value, t_print *print, int base, int cap)
+{
+	if (value->minus)
+	{
+		if (value->precision > print->size)
+			ft_print_zero(value->precision - print->size);
+		ft_puthex_fd(print->u, 1, base, cap);
+		if (value->width - ft_max(value->precision, print->size) > 0)
+			ft_print_space(value->width - ft_max(value->precision, print->size));
+	}
+	else
+	{
+		if (value->zero)
+			if (value->width - ft_max(value->precision, print->size) > 0)
+				ft_print_zero(value->width - ft_max(value->precision, print->size));
+		if (!value->zero)
+			if (value->width - ft_max(value->precision, print->size) > 0)
+				ft_print_space(value->width - ft_max(value->precision, print->size));
+		if (value->precision > print->size)
+			ft_print_zero(value->precision - print->size);
+		ft_puthex_fd(print->u, 1, base, cap);
+	}
+	return (ft_max(value->width, ft_max(value->precision, print->size)));
+}
+
+
 int		ft_put(t_flags *value, t_print *print)
 {
 	int i;
@@ -44,12 +70,14 @@ int		ft_put(t_flags *value, t_print *print)
 		i += ft_print_char(value, print);
 	else if (print->type == 's')
 		i += ft_print_str(value, print);
-	// else if (print->type == 'd' || print->type == 'i')
-	// 	i += ft_print_int(value, print);
-	// else if (print->type == 'u')
-	// 	i += ft_print_uint(value, print);
-	// else if (print->type == 'x' || print->type == 'X')
-	// 	i += ft_print_xint(value, print);
+	else if (print->type == 'd' || print->type == 'i')
+		i += ft_print_int(value, print);
+	else if (print->type == 'u')
+		i += ft_print_uint(value, print, 10, 0);
+	else if (print->type == 'x')
+		i += ft_print_uint(value, print, 16, 0);
+	else if (print->type == 'X')
+		i += ft_print_uint(value, print, 16, 1);
 	// else if (print->type == 'p')
 	// 	i += ft_print_ptr(value, print);
 	return (i);
